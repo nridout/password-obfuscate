cd80dada9b53057805225059df5f753e06087ce5
Nicole Ridout <nicole.ridout@gmail.com>
2019-02-10 22:27:30 +0000

    refactored code to use forEach instead of a for loop

--- password.js
+++ password.js
@@ -1,41 +1,31 @@
-//Write a program named password.js that
-//takes a single string as a command line argument.
+// takes a single string as a command line argument.
 var passwordInput = process.argv[2];
-
-
-//Within your program, write a function definition for a function named obfuscate
-//that takes a string as an argument and obfuscates it by applying the following rules.
+// function named obfuscate that obfuscates input by applying the following rules:
 function obfuscate(passwordInput) {
-
+  var letterArray = passwordInput.split("");
   var output = "";
-
-  for (var i = 0; i < passwordInput.length; i++) {
-
-    //Every "a" in the string should be replaced with a "4".
-    if (passwordInput[i] === "a") {
+  letterArray.forEach(function(letter) {
+    // Every "a" in the string should be replaced with a "4".
+    if (letter === "a") {
       output += "4";
     }
-    //Every "e" in the string should be replaced with a "3".
-    else if (passwordInput[i] === "e") {
+    // Every "e" in the string should be replaced with a "3".
+    else if (letter === "e") {
       output += "3";
     }
-    //Every "o" (oh) in the string should be replaced with a "0" (zero).
-    else if (passwordInput[i] === "o") {
+    // Every "o" (oh) in the string should be replaced with a "0" (zero).
+    else if (letter === "o") {
       output += "0";
     }
-    //Every "l" (el) in the string should be replaced with a "1" (one).
-    else if (passwordInput[i] === "l") {
+    // Every "l" (el) in the string should be replaced with a "1" (one).
+    else if (letter === "l") {
       output += "1";
     }
     else {
-      output += passwordInput[i];
+      output += letter;
     }
-
-  }
-
+  });
   return output;
-
 }
-
-//The program should log to the console the final obfuscated password returned by your obfuscate function.
+// The program should log to the console the final obfuscated password returned by your obfuscate function.
 console.log(obfuscate(passwordInput));
